9c453843bed61d4aeeb9834a00eb8813613dd91c
AlexKharenko <alexey16052002@gmail.com>
2022-04-12 14:55:11 +0300

    add router

--- ChatBack/index.js
+++ ChatBack/index.js
@@ -1,15 +1,17 @@
 const http = require('http');
 require('dotenv').config();
 
+const Router = require('./src/router');
+
 const server = http.createServer(async (req, res) => {
-    console.log(req, res);
-    res.end('Hello world!');
+    // console.log(req, res);
+    Router.route(req, res);
 });
 
 server.listen(process.env.PORT, process.env?.HOSTNAME, () => {
     console.log(
-        `Server is listening on ${process.env?.HOSTNAME || 'localhost'}:${
-            process.env.PORT
-        }`
+        `Server is listening on http://${
+            process.env?.HOSTNAME || 'localhost'
+        }:${process.env.PORT}`
     );
 });
