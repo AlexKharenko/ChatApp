ae8d545c7e4f11ff4e7a23cd557fd87e39853f36
AlexKharenko <alexey16052002@gmail.com>
2022-06-10 12:52:32 +0300

    added controller in index file

--- ChatBack/src/controllers/index.js
+++ ChatBack/src/controllers/index.js
@@ -2,10 +2,12 @@ const AuthController = require('./auth.controller');
 const ChatController = require('./chat.controller');
 const UserController = require('./user.controller');
 const MessageController = require('./message.controller');
+const SearchController = require('./search.controller');
 
 module.exports = {
     AuthController,
     ChatController,
     UserController,
     MessageController,
+    SearchController,
 };
